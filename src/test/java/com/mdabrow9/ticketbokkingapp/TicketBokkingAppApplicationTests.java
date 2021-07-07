56fb62a3a0aa715e0e7f563e1e7827c1986ebfed
Michal <czesio007p@gmail.com>
2021-07-07 13:57:37 +0200

    Initial commit

---
+++ src/test/java/com/mdabrow9/ticketbokkingapp/TicketBokkingAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mdabrow9.ticketbokkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBokkingAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
